f2ad04d43557d4ddc935c01f8187e5fba058d70c
nirmanov <vladislav.nirmanov@yandex.ru>
2023-02-15 08:07:43 +0300

    first commit

---
+++ _tests_/persents.test.js
@@ -0,0 +1,12 @@
+
+import { getPercents } from "../percents";
+
+describe("Проверка рассчёта процента от числа", () => {
+    it("10% от 100 = 10", () => expect(getPercents(10, 100)).toBe(10));
+    it("15% от 30 = 4.5", () => expect(getPercents(15, 30)).toBe(4.5));
+    it("6% от 70 = 4.2", () => expect(getPercents(6, 70)).toBe(4.2));
+    it("0% от 10 = 0", () => expect(getPercents(0, 10)).toBe(0));
+    it("-7% от 50 = error", () => expect(getPercents(-7, 50)).toBe("Процент не может быть меньше нуля."));
+    it("30% от -90 = error", () => expect(getPercents(30, -90)).toBe("Ожидается положительное число."));
+    it("120% от 34 = error", () => expect(getPercents(120, 34)).toBe("Процент не может быть больше ста."));
+});
